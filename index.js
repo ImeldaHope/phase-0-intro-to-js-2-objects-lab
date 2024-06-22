ff8e0d1d05e756937e8330382a8e9168b90e461b
Imelda Hope <imeldalarusso@gmail.com>
2024-06-22 23:18:12 +0300

    Modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name : "Hope",
+    streetAddress : "Nairobi",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {... obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"name","Susan");
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+    return employee; 
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"name", "Ruth");
+
+function deleteFromEmployeeByKey(obj,key) {
+    const newEmp = {...obj};
+    delete newEmp[key];
+
+    return newEmp;    
+}
+
+deleteFromEmployeeByKey(employee,"streetAddress");
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee,"name");
